3ff06caf809a1f53aa6988f2703f22b367d75ba5
DESKTOP-OA6AHKT\j9712 <j971229@gmail.com>
2024-07-05 02:53:16 +0900

    240705 lesson07 quiz02 문제풀기

--- src/main/java/com/quzi/lesson07/Lesson07Quiz02RestController.java
+++ src/main/java/com/quzi/lesson07/Lesson07Quiz02RestController.java
@@ -23,4 +23,34 @@ public class Lesson07Quiz02RestController {
 		return recruitRepository.findById(8);
 	}
 	
+	// http://localhost:8080/lesson07/quiz02/2
+	@GetMapping("/2")
+	public List<RecruitEntity> select2() {
+		return recruitRepository.findTop3ByOrderById();
+	}
+	
+	// http://localhost:8080/lesson07/quiz02/3
+	@GetMapping("/3")
+	public List<RecruitEntity> select3() {
+		return recruitRepository.findByPositionAndType("웹 back-end 개발자", "정규직");
+	}
+	
+	// http://localhost:8080/lesson07/quiz02/4
+	@GetMapping("/4")
+	public List<RecruitEntity> select4() {
+		return recruitRepository.findByTypeOrSalary("정규직", 9000);
+	}
+	
+	// http://localhost:8080/lesson07/quiz02/5
+	@GetMapping("/5")
+	public List<RecruitEntity> select5() {
+		return recruitRepository.findTop3ByOrderBySalaryDesc();
+	}
+	
+	// http://localhost:8080/lesson07/quiz02/6
+//	@GetMapping("/6")
+//	public List<RecruitEntity> select6() {
+//		
+//	}
+	
 }
--- src/main/java/com/quzi/lesson07/repository/RecruitRepository.java
+++ src/main/java/com/quzi/lesson07/repository/RecruitRepository.java
@@ -9,5 +9,9 @@ import com.quzi.lesson07.entity.RecruitEntity;
 public interface RecruitRepository extends JpaRepository<RecruitEntity, Integer> {
 	
 	public List<RecruitEntity> findById(int id);
+	public List<RecruitEntity> findTop3ByOrderById();
+	public List<RecruitEntity> findByPositionAndType(String position, String type);
+	public List<RecruitEntity> findByTypeOrSalary(String type, int salary);
+	public List<RecruitEntity> findTop3ByOrderBySalaryDesc();
 	
 }
